e62e101083f4af8056c395c99e83f37a84f29fff
lewisdaly <lewis@vesselstech.com>
2020-04-18 03:50:39 +0000

    merging 2 docker-compose files

--- docker/wait-for/wait-for-central-ledger.sh
+++ docker/wait-for/wait-for-central-ledger.sh
@@ -4,7 +4,7 @@ echo "** STARTUP - Checking for Central-Ledger..."
 
 source /opt/wait-for/wait-for.env
 
-sh /opt/wait-for/wait-for-mysql.sh
+sh /opt/wait-for/wait-for-mysql-central-ledger.sh
 
 sh /opt/wait-for/wait-for-kafka.sh
 
