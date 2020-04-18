20f117f7d0135d195f08b3e5c975dd2a33213cba
lewisdaly <lewis@vesselstech.com>
2020-04-18 06:50:22 +0000

    add note about failures

--- scripts/_test_transfer.sh
+++ scripts/_test_transfer.sh
@@ -7,6 +7,11 @@ POSTMAN_DIR=${DIR}/../../postman
 
 cd ${POSTMAN_DIR}
 
+
+# This is not quite working, there is some issue with the ALS/Oracle/Simulator config that is broken here. 
+# ths issue is likely in the `env_docker_compose_local.json` file, which sets up the config for the newman
+# tests
+
 newman run \
   --delay-request=2000 \
   --folder='p2p_money_transfer' \
