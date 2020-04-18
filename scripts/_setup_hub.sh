deb0cbc5564c461cb2f59aaf429f0f4a17c92ef4
lewisdaly <lewis@vesselstech.com>
2020-04-18 06:47:43 +0000

    adding setup scripts

--- scripts/_setup_hub.sh
+++ scripts/_setup_hub.sh
@@ -3,12 +3,47 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # point this to your mojaloop postman directory
-POSTMAN_DIR=${DIR}/../postman
+POSTMAN_DIR=${DIR}/../../postman
 
 cd ${POSTMAN_DIR}
 
 newman run \
   --delay-request=2000 \
   --folder='Hub Account' \
-  --environment=environments/Mojaloop-Local-Docker-Compose.postman_environment.json \
+  --environment=${DIR}/env_docker_compose_local.json \
   OSS-New-Deployment-FSP-Setup.postman_collection.json
+
+newman run \
+  --delay-request=2000 \
+  --folder='payerfsp (p2p transfers)' \
+  --environment=${DIR}/env_docker_compose_local.json \
+  OSS-New-Deployment-FSP-Setup.postman_collection.json
+
+newman run \
+  --delay-request=2000 \
+  --folder='payeefsp (p2p transfers)' \
+  --environment=${DIR}/env_docker_compose_local.json \
+  OSS-New-Deployment-FSP-Setup.postman_collection.json
+
+newman run \
+  --delay-request=2000 \
+  --folder='payeefsp' \
+  --environment=${DIR}/env_docker_compose_local.json \
+  OSS-New-Deployment-FSP-Setup.postman_collection.json
+
+# Hmm, looks like user 27720201001 is never registered...
+
+newman run \
+  --delay-request=2000 \
+  --folder='Oracle Onboarding' \
+  --environment=${DIR}/env_docker_compose_local.json \
+  OSS-New-Deployment-FSP-Setup.postman_collection.json
+
+newman run \
+  --delay-request=2000 \
+  --folder='Add Users to ALS / mock-pathfinder' \
+  --environment=${DIR}/env_docker_compose_local.json \
+  OSS-New-Deployment-FSP-Setup.postman_collection.json
+
+
+
--- scripts/_test_transfer.sh
+++ scripts/_test_transfer.sh
@@ -1,44 +1,17 @@
 #!/usr/bin/env bash
 
-# register the user
-# curl --location --request POST 'http://localhost:8444/payeefsp/parties/MSISDN/27713803912' \
-# --header 'Content-Type: application/json' \
-# --data-raw '{
-#     "party": {
-#         "partyIdInfo": {
-#             "partyIdType": "MSISDN",
-#             "partyIdentifier": "27713803912",
-#             "fspId": "payeefsp"
-#         },
-#         "name": "Siabelo Maroka",
-#         "personalInfo": {
-#             "complexName": {
-#                 "firstName": "Siabelo",
-#                 "lastName": "Maroka"
-#             },
-#             "dateOfBirth": "3/3/1973"
-#         }
-#     }
-# }'
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-## register with ALS
+# point this to your mojaloop postman directory
+POSTMAN_DIR=${DIR}/../../postman
 
-curl --location --request POST 'http://localhost:4002/participants/MSISDN/27713803912' \
---header 'Accept: application/vnd.interoperability.participants+json;version=1' \
---header 'Connection: keep-alive' \
---header 'Content-Type: application/vnd.interoperability.participants+json;version=1.0' \
---header 'Date: Fri, 21 Dec 2018 12:17:01 GMT' \
---header 'FSPIOP-Source: payeefsp' \
---header 'accept-encoding: gzip, deflate' \
---header 'cache-control: no-cache' \
---data-raw '{
-    "fspId": "payeefsp",
-    "currency": "USD"
-}'
+cd ${POSTMAN_DIR}
 
-# do a lookup?
-# curl --location --request GET 'http://localhost:4002/parties/MSISDN/11112222' \
-# --header 'Accept: application/vnd.interoperability.parties+json' \
-# --header 'Content-Type: application/vnd.interoperability.parties+json' \
-# --header 'FSPIOP-Source: payerfsp' \
-# --header 'Date: Thu, 24 Jan 2019 10:22:12 GMT'
+newman run \
+  --delay-request=2000 \
+  --folder='p2p_money_transfer' \
+  --environment=${DIR}/env_docker_compose_local.json \
+  Golden_Path.postman_collection.json
+
+
+
