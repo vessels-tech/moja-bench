08fbcdae637cf2040cc156b34aca8c1493922424
lewisdaly <lewis@vesselstech.com>
2020-04-20 15:46:03 +0000

    working on compose confug

--- scripts/_test_transfer.sh
+++ scripts/_test_transfer.sh
@@ -12,11 +12,43 @@ cd ${POSTMAN_DIR}
 # ths issue is likely in the `env_docker_compose_local.json` file, which sets up the config for the newman
 # tests
 
+# newman run \
+#   --delay-request=2000 \
+#   --folder='p2p_money_transfer' \
+#   --environment=${DIR}/env_docker_compose_local.json \
+#   Golden_Path.postman_collection.json
+
+# newman run \
+#   --delay-request=2000 \
+#   --folder='Run-sample-quotes-transfers-to-prep-simulator' \
+#   --environment=${DIR}/env_docker_compose_local.json \
+#   Golden_Path.postman_collection.json
+
+# newman run \
+#   --delay-request=2000 \
+#   --folder='p2p_happy_path SEND QUOTE' \
+#   --environment=${DIR}/env_docker_compose_local.json \
+#   Golden_Path.postman_collection.json
+
 newman run \
   --delay-request=2000 \
-  --folder='p2p_money_transfer' \
+  --folder='Get Party Receiver' \
   --environment=${DIR}/env_docker_compose_local.json \
   Golden_Path.postman_collection.json
+
+# curl --location --request GET 'http://localhost:4002/parties/MSISDN/27713803912' \
+# --header 'Accept: application/vnd.interoperability.parties+json;version=1' \
+# --header 'Content-Type: application/vnd.interoperability.parties+json;version=1.0' \
+# --header 'FSPIOP-Source: payerfsp' \
+# --header 'Date: Thu, 24 Jan 2019 10:22:12 GMT'
+
+
+# curl --location --request GET 'http://localhost:8444/payeefsp/requests/27713803912'
+# --header 'Accept: application/vnd.interoperability.parties+json;version=1' \
+# --header 'Content-Type: application/vnd.interoperability.parties+json;version=1.0' \
+# --header 'FSPIOP-Source: payerfsp' \
+# --header 'Date: Thu, 24 Jan 2019 10:22:12 GMT'
+
 
 
 
